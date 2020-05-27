01b563d5757ba952b93302991c6173e99f488396
Giordano Tamburrelli <tambug@gmail.com>
2020-05-27 17:26:58 +0200

    refactoring

--- src/main/kotlin/truelayer/data/v1/DataAPI.kt
+++ src/main/kotlin/truelayer/data/v1/DataAPI.kt
@@ -5,29 +5,30 @@ import truelayer.data.rest.v1.V1APIClient
 import truelayer.data.v1.domain.*
 
 class DataAPI(
-        private val V1APIClient: V1APIClient) {
+        private val V1APIClient: V1APIClient,
+        private val accessToken: AccessToken) {
 
-    fun getMetaDataFor(accessToken: AccessToken): AccessTokenMetaData {
+    fun getMetaDataFor(): AccessTokenMetaData {
         return V1APIClient.getMetaDataFor(accessToken)
     }
 
-    fun getIdentityInfo(accessToken: AccessToken): IdentityInfo {
+    fun getIdentityInfo(): IdentityInfo {
         return V1APIClient.getIdentityInfo(accessToken).identityInfoList[0]
     }
 
-    fun getAccounts(accessToken: AccessToken): List<Account> {
+    fun getAccounts(): List<Account> {
         return V1APIClient.getAccounts(accessToken).accountsList
     }
 
-    fun getAccount(accessToken: AccessToken, accountId: String): Account {
+    fun getAccount(accountId: String): Account {
         return V1APIClient.getAccount(accessToken, accountId).accountsList[0]
     }
 
-    fun getBalance(accessToken: AccessToken, accountId: String): Balance{
+    fun getBalance(accountId: String): Balance {
         return V1APIClient.getBalance(accessToken, accountId).balanceList[0]
     }
 
-    fun getTransactions(accessToken: AccessToken, accountId: String): List<Transaction>{
+    fun getTransactions(accountId: String): List<Transaction> {
         return V1APIClient.getTransactions(accessToken, accountId).transactionList
     }
 }
--- src/test/kotlin/truelayer/data/v1/DataAPITest.kt
+++ src/test/kotlin/truelayer/data/v1/DataAPITest.kt
@@ -5,7 +5,7 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
-import truelayer.data.auth.domain.*
+import truelayer.data.auth.domain.AccessToken
 import truelayer.data.rest.v1.V1APIClient
 import truelayer.data.v1.domain.*
 import kotlin.test.assertEquals
@@ -14,21 +14,21 @@ class DataAPITest {
 
     private lateinit var dataAPI: DataAPI
     private lateinit var v1APIClient: V1APIClient
+    private var accessToken = AccessToken("", "", "", "")
 
     @Before
     fun setUp() {
         v1APIClient = mockk()
-        dataAPI = DataAPI(v1APIClient)
+        dataAPI = DataAPI(v1APIClient, accessToken)
     }
 
     @Test
     fun `get metadata`() {
-        val accessToken = buildSomeAccessToken()
         val accessTokenMetaData = mockk<AccessTokenMetaData>()
 
         every { v1APIClient.getMetaDataFor(accessToken) } returns accessTokenMetaData
 
-        val result = dataAPI.getMetaDataFor(accessToken)
+        val result = dataAPI.getMetaDataFor()
 
         verify(exactly = 1) { v1APIClient.getMetaDataFor(accessToken) }
         assertEquals(result, accessTokenMetaData)
@@ -36,14 +36,13 @@ class DataAPITest {
 
     @Test
     fun `get identity info`() {
-        val accessToken = buildSomeAccessToken()
         val identityInfoResult = mockk<IdentityInfoResult>()
         val identityInfo = mockk<IdentityInfo>()
 
         every { identityInfoResult.identityInfoList[0] } returns identityInfo
         every { v1APIClient.getIdentityInfo(accessToken) } returns identityInfoResult
 
-        val result = dataAPI.getIdentityInfo(accessToken)
+        val result = dataAPI.getIdentityInfo()
 
         verify(exactly = 1) { v1APIClient.getIdentityInfo(accessToken) }
         assertEquals(result, identityInfo)
@@ -51,14 +50,13 @@ class DataAPITest {
 
     @Test
     fun `get account list`() {
-        val accessToken = buildSomeAccessToken()
         val accounts = mockk<Accounts>()
         val accountList = mockk<List<Account>>()
 
         every { v1APIClient.getAccounts(accessToken) } returns accounts
         every { accounts.accountsList } returns accountList
 
-        val result = dataAPI.getAccounts(accessToken)
+        val result = dataAPI.getAccounts()
 
         verify(exactly = 1) { v1APIClient.getAccounts(accessToken) }
         assertEquals(result, accountList)
@@ -66,7 +64,6 @@ class DataAPITest {
 
     @Test
     fun `get account`() {
-        val accessToken = buildSomeAccessToken()
         val accounts = mockk<Accounts>()
         val account = mockk<Account>()
         val id = "fakeid"
@@ -74,11 +71,9 @@ class DataAPITest {
         every { v1APIClient.getAccount(accessToken, id) } returns accounts
         every { accounts.accountsList[0] } returns account
 
-        val result = dataAPI.getAccount(accessToken, id)
+        val result = dataAPI.getAccount(id)
 
         verify(exactly = 1) { v1APIClient.getAccount(accessToken, id) }
         assertEquals(result, account)
     }
-
-    private fun buildSomeAccessToken() = AccessToken("", "", "", "")
 }
